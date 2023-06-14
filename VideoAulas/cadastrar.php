982c4149b88ca3762967e81e986e7fbfed8b3a69
Nog <nogueiraandrea816@gmail.com>
2023-06-14 09:34:31 -0300

    inicio formulário cadastrar

---
+++ VideoAulas/cadastrar.php
@@ -0,0 +1,9 @@
+<h1>Cadastrar usuário<h1>
+<form method-"POST">
+    <label>
+        Nome: <input type="text" name="nome" />>
+    </label>
+    <label>
+        Idade: <input type="text" name="idade" />>
+    </label>
+</form>
